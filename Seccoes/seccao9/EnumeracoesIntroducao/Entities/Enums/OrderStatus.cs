c35cf91d825c02425d57bc7a9f251cc1af753409
Alex Marques <aabfcm@gmail.com>
2024-09-11 18:28:34 +0100

    Added exercice from section 9

---
+++ Seccoes/seccao9/EnumeracoesIntroducao/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnumeracoesIntroducao.Entities.Enums {
+     enum OrderStatus : int { //cada valor do meu order status vai ser to tipo inteiro
+                              // esta syntax dos ":" vou apredner melhor no cpatiulo sobre herança
+                              // é derivado do tipo base que é um int
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered =3
+
+
+    }
+}
---
+++ Seccoes/seccao9/EnumeracoesIntroducao/Entities/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using EnumeracoesIntroducao.Entities.Enums;
+
+namespace EnumeracoesIntroducao.Entities {
+    class Order {
+        public int Id { get; set; }
+        public DateTime Moment {  get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString() {
+            return Id
+                + ", "
+                + Moment
+                + ", "
+                + Status;
+                
+        }
+
+    }
+}
